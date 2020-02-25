4f3e48ca73e826b534b7cf953c259c1fef89eeb8
paul handcock <phandcock@live.com.au>
2020-02-25 21:29:54 +0800

    Tweaks Color updates Add missing file

---
+++ GrampsView/UserControls/FormsRadioButton/RadioItemToggledEventArgs.cs
@@ -0,0 +1,14 @@
+﻿namespace GrampsView.UserControls
+{
+    using System;
+
+    public class RadioItemToggledEventArgs : EventArgs
+    {
+        public RadioItemToggledEventArgs(RadioItem selectedItem)
+        {
+            SelectedItem = selectedItem;
+        }
+
+        public RadioItem SelectedItem { get; }
+    }
+}
